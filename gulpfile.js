17c89a8da5f8c0d108f20d80409c8497c3b033aa
Ian Liao <ian110652@gmail.com>
2015-09-05 15:08:59 +0800

    依序執行 mytask2, mytask1, default 工作

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 
-gulp.task('default',['task1', 'task2'], function() {
+gulp.task('default',['task1'], function() {
     console.log('Hello, Gulp!');
 });
 
-gulp.task('task1', function() {
+gulp.task('task1',['task2'], function(callback) {
     console.log('Task1 Output...');
+    callback();
 });
 
-gulp.task('task2', function() {
+gulp.task('task2', function(callback) {
     console.log('Task2, Output...');
+    callback();
 });
