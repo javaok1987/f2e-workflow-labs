8becfd0262929370de84275292458ad20063299a
Ian Liao <ian110652@gmail.com>
2015-09-05 15:31:49 +0800

    將所有 assets/vendor/ 目錄下 (含子目錄) 所有 *.js 與 *.css 檔案全部輸出到 output3 資料夾下

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 
-gulp.task('default',['task1'], function() {
+gulp.task('default', ['task1'], function() {
     console.log('Hello, Gulp!');
 });
 
-gulp.task('task1',['task2'], function(callback) {
+gulp.task('task1', ['task2'], function(callback) {
     console.log('Task1 Output...');
     callback();
 });
@@ -12,4 +12,25 @@ gulp.task('task1',['task2'], function(callback) {
 gulp.task('task2', function(callback) {
     console.log('Task2, Output...');
     callback();
+});
+
+gulp.task('output1', function() {
+    gulp.src('assets/vendor/bootstrap/**/*.js')
+        .pipe(gulp.dest('output1'));
+});
+
+gulp.task('output2', function() {
+    gulp.src('assets/vendor/bootstrap/**/*.js', {
+            base: 'assets'
+        })
+        .pipe(gulp.dest('output2'));
+});
+
+gulp.task('output3', function() {
+    gulp.src(['assets/vendor/**/*.js',
+            'assets/vendor/**/*.css'
+        ], {
+            base: 'assets/vendor/'
+        })
+        .pipe(gulp.dest('output3'));
 });
